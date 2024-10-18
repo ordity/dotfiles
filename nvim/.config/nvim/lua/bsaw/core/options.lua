d891b78dd003cb691627ec4b5f3259a1e47e4c17
ordity <bartek.sawko@gmail.com>
2024-10-18 21:24:45 +0200

    Update i3wm and nvim configs

--- nvim/.config/nvim/lua/bsaw/core/options.lua
+++ nvim/.config/nvim/lua/bsaw/core/options.lua
@@ -1,37 +1,39 @@
 vim.cmd("let g:netrw_liststyle = 3")
 
+-- clean up the configs a little bit
 local opt = vim.opt
 
+-- row number settings
 opt.relativenumber = true
 opt.number = true
 
+-- indent settings
 opt.tabstop = 4 -- 4 spaces for tabs (prettier default)
 opt.shiftwidth = 4 -- 4 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
-opt.wrap = false
+opt.wrap = false -- wrap lines
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+opt.smartcase = true -- assumes case-sensitive for search when mixing cases
 
-opt.cursorline = false
+opt.cursorline = true
 
--- turn on termguicolors for tokyonight colorscheme to work
--- (have to use iterm2 or any other true color terminal)
+-- colorscheme settings
 opt.termguicolors = false -- use terminal colors for GUI
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 opt.scrolloff = 8 -- scroll if you are 8 characters over the EoF
 opt.isfname:append("@-@") -- add @-@ to the possible filenames
 
-opt.colorcolumn = "89" -- add a colored background for 80th column
+opt.colorcolumn = "100" -- add a colored background for 100th column
 
 opt.updatetime = 50
 
 -- backspace
-opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+opt.backspace = "indent,eol,start" -- backspace on indent, end of line or insert mode start position
 
 -- clipboard
 opt.clipboard:append("unnamedplus") -- use system clipboard as default register
