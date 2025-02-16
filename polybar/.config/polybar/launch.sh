d14acc20309f6d1378d9aae495f653c2623f20c2
ordity <bartek.sawko@gmail.com>
2025-02-16 03:05:48 +0100

    Update 16-02-2025

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
+# If all your bars have ipc enabled, you can use
 polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+# I'm not sure why this line is here. I'm leaving it commented out just in case.
+# echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+if type "xrandr"; then # check if ranr exists
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do # grab monitor outputs
     MONITOR=$m polybar --reload main_bar &
   done
 else
